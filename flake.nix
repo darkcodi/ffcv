3690b4e8d08da98f7833b6cb655ed1acf4b4a8f9
darkcodi <trooper982@gmail.com>
2026-01-03 22:52:55 -0600

    Add flake.nix and flake.lock

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "ffcv - Firefox Configuration Viewer";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.ffcv = pkgs.rustPlatform.buildRustPackage {
+          pname = "ffcv";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          buildType = "release";
+        };
+
+        defaultPackage = self.packages.${system}.ffcv;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.rustc pkgs.cargo ];
+        };
+      });
+}
